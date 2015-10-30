8625af2388b7e966dded563158a8cb89bdb7420c
Xinran <lindashi527@gmail.com>
2015-10-30 14:17:12 -0700

    Initial Commit

---
+++ Attributor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Attributor
+//
+//  Created by Kaiqi on 10/30/15.
+//  Copyright © 2015 self.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
