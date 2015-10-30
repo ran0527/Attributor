c907c2058f8196e960d4d730a390204ee3032a08
Xinran <lindashi527@gmail.com>
2015-10-30 15:02:46 -0700

    init Attributor

--- Attributor/ViewController.m
+++ Attributor/ViewController.m
@@ -9,10 +9,32 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UITextView *body;
+@property (weak, nonatomic) IBOutlet UILabel *headline;
 @end
 
 @implementation ViewController
+- (IBAction)changeBodySelectionColorToMatchBackgroundOfButton:(UIButton *)sender
+{
+    [self.body.textStorage addAttribute:NSForegroundColorAttributeName
+                                  value:sender.backgroundColor
+                                  range:self.body.selectedRange];
+}
+
+- (IBAction)outlineSelectionBodySelection:(id)sender
+{
+    [self.body.textStorage addAttributes:@{ NSStrokeWidthAttributeName: @-3,
+                                            NSStrokeColorAttributeName: [UIColor blackColor] }
+                                   range:self.body.selectedRange];
+}
+
+- (IBAction)unoutlineBodySelection:(id)sender
+{
+    [self.body.textStorage removeAttribute:NSStrokeColorAttributeName
+                                     range:self.body.selectedRange];
+}
+
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
